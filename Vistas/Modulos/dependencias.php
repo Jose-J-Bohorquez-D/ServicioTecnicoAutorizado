66b03c03b4bc1f12aba7565a3091d578393d8c76
Jose Bohorquez <bd567358546@gmail.com>
2023-07-19 16:15:02 -0500

    ajustamos la plantilla completamente al MVC

---
+++ Vistas/Modulos/dependencias.php
@@ -0,0 +1,15 @@
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="Vistas/Dependencias/Plantilla/plugins/fontawesome-free/css/all.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="Vistas/Dependencias/Plantilla/dist/css/adminlte.min.css">
+
+<!-- jQuery -->
+<script src="Vistas/Dependencias/Plantilla/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="Vistas/Dependencias/Plantilla/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="Vistas/Dependencias/Plantilla/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="Vistas/Dependencias/Plantilla/dist/js/demo.js"></script>
