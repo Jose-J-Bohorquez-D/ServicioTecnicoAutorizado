ccef136ca93482de05a3053f9a239d926b0b97fe
Jose Bohorquez <bd567358546@gmail.com>
2023-07-19 15:55:35 -0500

    agregamos la plantilla de adminlte y ajustamos el MVC inicial

---
+++ Controladores/Controlador.Mvc.php
@@ -0,0 +1,11 @@
+<?php 
+
+	class ControladorMvc
+	{
+		public function inclusion_plantilla()
+		{
+			include('Vistas/Plantilla.php');
+		}
+	}
+
+ ?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+	include('Controladores/Controlador.Mvc.php');
+	$mvc = new ControladorMvc; $mvc->inclusion_plantilla();
+?>
